5bb18f31efa16612db9e622b221932f38c945adb
wikkhaira <waqassth23@gmail.com>
2022-12-14 14:52:25 +0500

    for checking files

--- mvc/views/error.php
+++ mvc/views/error.php
@@ -5,7 +5,7 @@
     <div class="error-content">
         <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
         <p>
-            We could not find the page you were looking for.
+            We could not find the page you were looking for. Thanks
             Meanwhile, you may <a href="<?=base_url('dashboard/index')?>">return to dashboard</a> or try using the search form.
         </p>
     </div><!-- /.error-content -->
